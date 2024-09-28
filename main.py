6378b4ac9557bcc558f393c25a108cad0d79fb22
Keith Jung <60eokk@gmail.com>
2024-09-27 20:18:20 -0500

    adding configurations are needed

--- main.py
+++ main.py
@@ -293,3 +293,6 @@ if __name__ == "__main__":
     sagemaker_predictor.delete_endpoint()
 
     # AWS Configuration
+    # Copyexport AWS_ACCESS_KEY_ID=
+    # export AWS_SECRET_ACCESS_KEY=
+    # export AWS_DEFAULT_REGION=
