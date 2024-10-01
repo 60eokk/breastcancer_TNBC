1adadb562c694bdbde26291bbbfef1fcf1c92462
Keith Jung <60eokk@gmail.com>
2024-09-30 19:23:40 -0500

    tensorflow models created

--- main.py
+++ main.py
@@ -312,7 +312,20 @@ tensorflow_model = TensorFlowModel(
     entry_point='serve.py'  # Create file in your project directory
 )
 
+def model_fn(model_dir):
+    model = tf.keras.models.load_model(model_dir + '/1')
+    return model
 
+def input_fn(request_body, request_content_type):
+    import numpy as np
+    return np.array(request_body, dtype=np.float32)
+
+def predict_fn(input_data, model):
+    return model.predict(input_data)
+
+def output_fn(prediction, content_type):
+    import json
+    return json.dumps(prediction.tolist())
 
 
 
