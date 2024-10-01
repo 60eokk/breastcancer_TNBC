3f748906b6c98c2ea56f88d8cb6cea8262147290
Keith Jung <60eokk@gmail.com>
2024-10-01 14:17:33 -0500

    serve.py file to correctly load the .h5 model

--- serve.py
+++ serve.py
@@ -1,13 +1,15 @@
 import tensorflow as tf
 import numpy as np
 import json
+import os
 
 def model_fn(model_dir):
-    model = tf.keras.models.load_model(model_dir + '/model.h5')
+    model_path = os.path.join(model_dir, 'model.h5')
+    model = tf.keras.models.load_model(model_path)
     return model
 
 def input_fn(request_body, request_content_type):
-    return np.array(request_body, dtype=np.float32)
+    return np.array(json.loads(request_body), dtype=np.float32)
 
 def predict_fn(input_data, model):
     return model.predict(input_data)
